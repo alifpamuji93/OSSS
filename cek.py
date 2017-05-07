8668a520667b05e4008ecf4805ed56995dde676d
Nur Wachid <wachid@outlook.com>
2017-05-07 17:37:26 +0700

    revert commit

--- cek.py
+++ cek.py
@@ -1,18 +1,5 @@
-from flask import Flask, request, url_for, redirect, render_template, Response
-from flask.ext.sqlalchemy import SQLAlchemy
-from datetime import datetime
+from model.camera import VideoCamera
 
-app = Flask(__name__)
+camera = VideoCamera()
 
-
-
-@app.route('/')
-def index():
-	return render_template('base.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	pass
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+camera.rekam()
--- model/__init__.py
+++ model/__init__.py
@@ -5,9 +5,6 @@ from __future__ import (
     division,
 )
 
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
 from .camera import (
     VideoCamera
 )
@@ -20,11 +17,3 @@ from .camera import (
 # from .kirim import (
 # 	mail
 # )
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:database.db'
-db = SQLAlchemy(app)
-
-from my_app.catalog.views import catalog
-app.register_blueprint(catalog)
-db.create_all()
