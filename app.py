4037abab976d90f8602fa5d2cadb504330e45d53
alifpamuji93 <alifpamuji93@gmail.com>
2017-05-08 03:50:08 +0000

    edit

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import RPi.GPIO as GPIO
 import time
 from model.lampu import lampu_on, lampu_off
 from model.kirim import mail
-<<<<<<< HEAD:pir_test2torelay_v2.py
-from model.kamera import rekam_on, rekam_off
-=======
 from model.camera import VideoCamera as camera
 
->>>>>>> refs/remotes/origin/master:app.py
 GPIO.setmode(GPIO.BCM)
 pirPin = 18
 GPIO.setup(pirPin, GPIO.IN)
@@ -24,11 +20,9 @@ try:
         else:
             print "Gerakan terdeteksi!"
             print "Kamera mulai merekam..."
-<<<<<<< HEAD:pir_test2torelay_v2.py
-            rekam_on()
-=======
+
             camera.rekam()
->>>>>>> refs/remotes/origin/master:app.py
+
             mail("alifpamuji93@gmail.com", "subjek", "halo", "README.md")
             time.sleep(3.0)
             lampu_on()
