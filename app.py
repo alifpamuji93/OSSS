5b970f88c0ffdecb38741313cec95ef6fce7cc17
Nur Wachid <wachid@outlook.com>
2017-05-30 08:31:16 +0000

    change schems of notify email

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import cv2
 import time
 from datetime import datetime
 from model.lampu import lampu_on, lampu_off
-#from model.kirim import mail
+from model.kirim import mail
 #from model.camera import VideoCamera as camera
 
 GPIO.setmode(GPIO.BCM)
@@ -15,25 +15,28 @@ try:
     while True:
         if GPIO.input(pirPin) == GPIO.HIGH:
             print ("Gerakan terdeteksi!")
+            mail("alifpamuji93@gmail.com", "subjek", "halo", "README.md")
             print ("Kamera mulai merekam...")
 
 
             cap = cv2.VideoCapture(0)
+            fps = 20
 
             filename = datetime.now().strftime("static/video/%Y-%m-%d_%H.%M.%S.avi")
             codec = cv2.VideoWriter_fourcc(*'XVID')
-            out = cv2.VideoWriter(filename, codec, 20.0, (640, 480))
+            out = cv2.VideoWriter(filename, codec, fps, (640, 480))
             ret, frame = cap.read()
-            delay = 300
+            delay = 20*fps
                
             while ret and delay > 0:
                 out.write(frame)
                 ret, frame = cap.read()
+                lampu_on()
                 delay -= 1
 
-#            mail("alifpamuji93@gmail.com", "subjek", "halo", "README.md")
+
 #                time.sleep(3.0)
-#                lampu_on()
+                
                 
 
 #                time.sleep(10.0)
